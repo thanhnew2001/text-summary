ffb5ad0285b6824259dae480d210857cd90d2aa6
thanhnew2001 <thanh.hispvietnam@gmail.com>
2024-04-14 12:43:59 +0700

    Update translate.py

--- translate.py
+++ translate.py
@@ -2,17 +2,15 @@ from hf_hub_ctranslate2 import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub
 
 
 def perform_translation(text, model_dir):
-        start_time = time.time()
-        model = TranslatorCT2fromHfHub(
+    model = TranslatorCT2fromHfHub(
             model_name_or_path=model_dir,
             device="cuda",
             compute_type="int8_float16",
             tokenizer=AutoTokenizer.from_pretrained(model_dir),
         )
 
-        translated_text = model.generate(text=text)
-        end_time = time.time()
-        return translated_text, end_time - start_time
+    translated_text = model.generate(text=text)
+    return translated_text, end_time - start_time
 
 
 perform_translation("Hello how are you", "en-vi")

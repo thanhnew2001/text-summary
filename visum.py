e6a3a263857308e98dc3b39a131408082aa2cb5d
thanhnew2001 <thanh.hispvietnam@gmail.com>
2024-04-14 07:36:10 +0700

    Update visum.py

--- visum.py
+++ visum.py
@@ -1,1 +1,23 @@
 
+from huggingface_hub import snapshot_download
+token = "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"
+
+snapshot_download(repo_id="Eugenememe/mix-en-vi-4m", token = token)
+
+from hf_hub_ctranslate2 import TranslatorCT2fromHfHub, GeneratorCT2fromHfHub
+from transformers import AutoTokenizer
+
+
+model_name = "/root/.cache/huggingface/hub/models--Eugenememe--mix-en-vi-4m/snapshots/f9735b6c75003a36bbabe9d992b8b05625b6b871"
+# use either TranslatorCT2fromHfHub or GeneratorCT2fromHfHub here, depending on model.
+model = TranslatorCT2fromHfHub(
+        # load in int8 on CUDA
+        model_name_or_path=model_name, 
+        device="cuda",
+        compute_type="int8_float16",
+        tokenizer=AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+)
+outputs = model.generate(
+    text=["How do you call a fast Flan-ingo?", "User: How are you doing?"],
+)
+print(outputs)

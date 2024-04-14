c81a647d426ba35e63436f95e9b372b06c0ada1c
thanhnew2001 <thanh.hispvietnam@gmail.com>
2024-04-14 08:36:41 +0700

    Create visumct2.py

---
+++ visumct2.py
@@ -0,0 +1,13 @@
+import ctranslate2
+import sentencepiece as spm
+
+sp = spm.SentencePieceProcessor()
+sp.load("source.spm")
+
+source = sp.encode("Hello world!", out_type=str)
+
+translator = ctranslate2.Translator("model2")
+results = translator.translate_batch([source])
+
+output = sp.decode(results[0].hypotheses[0])
+print(output)
